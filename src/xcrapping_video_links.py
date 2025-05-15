899101ed59e03a578d57c7ee2f085cf4a05fd54d
sissygsm <trgfbvtrgfnb@gmail.com>
2025-05-14 23:37:17 -0500

    Update xcrapping_video_links.py
    Refactoring  all functions

--- src/xcrapping_video_links.py
+++ src/xcrapping_video_links.py
@@ -25,66 +25,55 @@ def scrap_links_withFirefox(username):
     if not os.path.exists(user_dir):
         os.makedirs(user_dir)
 
-    # Use your Firefox profile
+    # Driver configurations
     profile_path = f"C:\\Users\\{PC_USER}\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\{FIREFOX_PROFILE}"
     fp = webdriver.FirefoxProfile(profile_path)
     options = webdriver.FirefoxOptions()
     options.profile = fp
-
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
-
-    url = f"https://x.com/{username}/media"
-
-    driver.get(url)
+    
+    driver.get(f"https://x.com/{username}/media")
     time.sleep(5)
 
-    tweet_links = []
-
-    keep_adding = True
-    while keep_adding:
-        keep_adding = False
-        links = driver.find_elements(By.TAG_NAME, "a")
-        for link in links:
-            href = link.get_attribute("href")
-            if href and (f"/{username}/status/" in href) and (href not in tweet_links):
-                tweet_links.append(href)
-                keep_adding = True
-
+    len_set, len_all_links = -1, 0
+    all_links = set()
+    while len_set != len_all_links:
+        len_set = len_all_links
+        elements = driver.find_elements(By.TAG_NAME, "a")
+        links = {e.get_attribute("href") for e in elements}
+        all_links |= links
+        len_all_links = len(all_links)
         driver.execute_script("""window.scrollTo(0, document.body.scrollHeight);
                             window.scrollTo(document.body.scrollHeight, 0);
-                            window.scrollTo(0, document.body.scrollHeight);
-                            """)
+                            window.scrollTo(0, document.body.scrollHeight);""")
         time.sleep(3)
-
-    print(f"\nTotal media tweet links found: {len(tweet_links)}")
+    
     driver.quit()
-    return tweet_links
+    return all_links
 
 
-def filter_links(tweet_links):
+def filter_links(x_links, username):
+    media_links = [l for l in x_links if f"/{username}/status/" in l]
+    print(f"\nTotal media links found: {len(tweet_links)}")
     # Filter by type (videos only)
-    video_links = [link for link in tweet_links if "/video/" in link]
-    print(f"\nTotal video tweet links found: {len(video_links)}")
-    # Cut the link to only show user + video number
+    video_links = [l for l in media_links if "/video/" in l]
+    print(f"\nTotal video links found: {len(video_links)}")
+    # Cut the link to only show https://x.com/user/video_number
     cleaned_links = [link[:-8] for link in video_links]
+    #cleaned_links = ["/".join(link.split("/")[:2]) for link in video_links]
+    cleaned_links.sort()
     return cleaned_links
 
 
 def save_pending_links(links, username, file="pending_links.txt"):
-    """Save a list of links to a pending file, without duplicates"""
     user_dir = os.path.join(DOWNLOADS_DIR, username)
     file_path = os.path.join(user_dir, file)
-    existing = set()
-    if os.path.exists(file_path):
-        with open(file_path, "r") as f:
-            existing = set([line.strip() for line in f])
-    new_links = [l for l in links if l not in existing]
     with open(file_path, "a") as f:
-        for link in new_links:
-            f.write(link + "\n")
-    print(f"{len(new_links)} new links saved to {file_path}")
+        for link in links: 
+            f.write(link + "\n")    
+    print(f"\n{len(links)} new links saved to {file_path}")
 
 
 x_links = scrap_links_withFirefox(username=X_USERNAME)
-video_links_only = filter_links(x_links)
+video_links_only = filter_links(x_links, X_USERNAME)
 save_pending_links(video_links_only, X_USERNAME)
