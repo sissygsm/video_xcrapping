4563f31defcabb8aeb5d8df80a2db9b8e6043387
sissygsm <trgfbvtrgfnb@gmail.com>
2025-05-22 00:48:25 -0500

    Update secrets.py
    Update secrets.py to replace placeholder values for Firefox profile and username with generic names for better clarity and usability.

--- src/secrets.py
+++ src/secrets.py
@@ -1,3 +1,9 @@
-PC_USER = 'your_pc'
-FIREFOX_PROFILE = 'your_profile' + '.default-release'
+# In Linux:
+#    Inside ~/snap/firefox/common/.mozilla/firefox/ 
+#    or ~/.mozilla/firefox/
+#
+#In Windows:
+#    In the folow place:
+#    C:\Users\your_user\AppData\Roaming\Mozilla\Firefox\Profiles\
+FIREFOX_PROFILE = 'your_profile_name' + '.default-release'
 X_USERNAME = "your_username"  # The @ username
