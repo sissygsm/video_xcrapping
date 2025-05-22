1acf98524406da28d1226cfb6edc69f84336b05d
sissygsm <trgfbvtrgfnb@gmail.com>
2025-05-21 21:16:19 -0500

    Update dling_videos_from_links.py

--- src/dling_videos_from_links.py
+++ src/dling_videos_from_links.py
@@ -5,16 +5,15 @@
 from yt_dlp import YoutubeDL
 from yt_dlp.utils import DownloadError
 
-import time
 import os
+import time
+
+import secrets
 
 # Create downloads directory if it doesn't exist
 DOWNLOADS_DIR = "./downloads"
 if not os.path.exists(DOWNLOADS_DIR):
     os.makedirs(DOWNLOADS_DIR)
-
-FIREFOX_PROFILE = 'your_profile_code' + '.default-release'
-X_USERNAME = "your_x_username"  # The @ username
 
 
 def download_pending_links(username, file="pending_links.txt"):
@@ -29,30 +28,33 @@ def download_pending_links(username, file="pending_links.txt"):
     # Get the url id number to name the mp4 file
     output_path = os.path.join(user_dir, f'%(original_url.-19:)s.%(ext)s')
     ydl_opts = {
-        'cookiesfrombrowser': ('firefox', FIREFOX_PROFILE),
+        'cookiesfrombrowser': ('firefox', secrets.FIREFOX_PROFILE),
         'outtmpl': output_path,
         'format': 'bestvideo+bestaudio/best',
         'merge_output_format': 'mp4',
         'noplaylist': True
     }
-    BATCH_SIZE = 50  # X/Twitter Rate-limit links to download 
-    while links:
-        len_batch = min(BATCH_SIZE, len(links))
-        batch = [links.pop() for _ in range(len_batch)]
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download(batch)
-        
-        with open(file_path, "w") as f:
-            f.write("\n".join(links) + "\n")
-            
-        if links is []:
-            print(f"All batchs downloaded.")
+    i = 0
+    while True:
+        l = links.pop()
+        try:
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download([l])
+        except DownloadError as e:
+            links.append(l)
+            i = 49
+        i += 1
+        if links == []:
             break
-        print(f"Batch downloaded. Rate-limit exceeded -> Wait 10 min or end script")
-        time.sleep(600)  # 10min = 600seg
-
+        if i % 50 == 0:  # X/Twitter Rate-limit links to download
+            print(f"Rate-limit exceeded -> Wait 10 min or end script")
+            with open(file_path, "w") as f:
+                f.write("\n".join(links) + "\n")
+            time.sleep(600)  # 10min = 600seg
+    
+    print(f"All videos was downloaded.")
     if os.path.exists(file_path):
         os.remove(file_path)
 
 
-download_pending_links(username=X_USERNAME)
+download_pending_links(username=secrets.X_USERNAME)
